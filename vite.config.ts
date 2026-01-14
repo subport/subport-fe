442ac3bc754294dc1ae791ca2735df4b9a4fddc8
Ajungmiiin <wjdals3249@naver.com>
2026-01-15 08:00:56 +0900

    feat : proxy 설정, svgr, mkcert 플러그인 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,40 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv, type ConfigEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import svgr from 'vite-plugin-svgr';
+import mkcert from 'vite-plugin-mkcert';
 import path from 'path';
+import fs from 'fs';
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+
+export default defineConfig(({ mode }: ConfigEnv) => {
+  const env = loadEnv(mode, process.cwd());
+  const isDev = mode === 'development';
+
+  return {
+    server: {
+      https: isDev
+        ? {
+            key: fs.readFileSync('localhost-key.pem'),
+            cert: fs.readFileSync('localhost.pem'),
+          }
+        : undefined,
+      proxy: isDev
+        ? {
+            '/api': {
+              target: env.VITE_API_URL,
+              changeOrigin: true,
+              secure: false,
+            },
+          }
+        : undefined,
     },
-  },
+
+    plugins: [react(), tailwindcss(), svgr(), mkcert()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  };
 });

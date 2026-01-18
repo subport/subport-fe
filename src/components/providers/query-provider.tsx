3ec142874d21e70c51986f7e6657011448bf844a
Ajungmiiin <wjdals3249@naver.com>
2026-01-19 08:21:29 +0900

    feat : query-provider 컴포넌트 생성

---
+++ src/components/providers/query-provider.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      retry: 0,
+    },
+  },
+});
+
+function QueryProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
+
+export default QueryProvider;

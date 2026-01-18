ca26c9f437169d7101a36b9e5fa53a80b5a4c1ee
Ajungmiiin <wjdals3249@naver.com>
2026-01-19 08:22:25 +0900

    feat : 공통 bottom-navigation 컴포넌트 추가

---
+++ src/components/layout/bottom-navigation.tsx
@@ -0,0 +1,31 @@
+import HomeIcon from '@/assets/icons/home-icon.svg?react';
+import AddIcon from '@/assets/icons/add-icon.svg?react';
+import MyPageIcon from '@/assets/icons/my-page-icon.svg?react';
+import { Link, useLocation } from 'react-router-dom';
+
+function BottomNavigation() {
+  const { pathname } = useLocation();
+  return (
+    <div className="bottom-nav absolute bottom-0 left-0 z-10 w-full rounded-[40px] bg-white/10 py-3 backdrop-blur-2xl">
+      <div className="flex items-center justify-center gap-12">
+        <Link to="/">
+          <HomeIcon
+            className="transition-colors"
+            fill={pathname === '/' ? '#6FCFC3' : '#ffffff'}
+          />
+        </Link>
+        <Link to="/add-subscribe" className="rounded-2xl bg-[#B2B4B6] p-2">
+          <AddIcon fill="#ffffff" />
+        </Link>
+        <Link to="/my">
+          <MyPageIcon
+            className="transition-colors"
+            fill={pathname === '/my' ? '#6FCFC3' : '#ffffff'}
+          />
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default BottomNavigation;

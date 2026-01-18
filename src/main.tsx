77679bfe3a319884b0d3092c3bc7aac8f0e6627b
Ajungmiiin <wjdals3249@naver.com>
2026-01-19 08:19:57 +0900

    feat : route 생성

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
-import { BrowserRouter } from 'react-router';
 
-createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-);
+import Router from './routes/index.tsx';
+
+createRoot(document.getElementById('root')!).render(<Router />);

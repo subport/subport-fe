b0459dcbd028b21740e95653935c988fb5cfcc66
Ajungmiiin <wjdals3249@naver.com>
2026-01-19 08:32:27 +0900

    feat : add-subscribe-form-page 추가

---
+++ src/routes/pages/add-subscribe-form-page.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import AddSubscribeForm from '@/components/subscribe/add-subscribe-form';
+
+function AddSubscribeFormPage() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // TODO : 구독 목록에 없는 ID라면 add-subscribe 페이지로 이동
+    if (!id) {
+      navigate('/add-subscribe');
+    }
+  }, [id, navigate]);
+
+  return (
+    <div className="flex h-full flex-col">
+      <p className="mr-auto mb-5 w-[50%] text-xl/loose font-semibold break-keep">
+        구독 서비스 정보를 입력해주세요
+      </p>
+
+      <AddSubscribeForm id={id as string} />
+    </div>
+  );
+}
+
+export default AddSubscribeFormPage;

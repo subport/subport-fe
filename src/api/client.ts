ada36401c0f80528002e49ef29ae92240139a16e
Ajungmiiin <wjdals3249@naver.com>
2026-01-19 08:13:50 +0900

    feat : axios 인스턴스 생성, refresh 인터셉터 로직 추가

---
+++ src/api/client.ts
@@ -0,0 +1,52 @@
+import { tokenStorage } from '@/lib/token-storage';
+import axios, { AxiosError, isAxiosError } from 'axios';
+import { refresh } from './auth';
+
+export const client = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+});
+
+client.interceptors.request.use(async (config) => {
+  const token = tokenStorage.getToken();
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+client.interceptors.response.use(
+  async (response) => {
+    console.log(response);
+
+    return response;
+  },
+  async (error: AxiosError) => {
+    const originalRequest = error.config as typeof error.config & {
+      _retry: boolean;
+    };
+
+    if (!isAxiosError(error)) {
+      return Promise.reject(error);
+    }
+
+    if (isAxiosError(error)) {
+      if (error.status === 401 && !originalRequest?._retry) {
+        originalRequest._retry = true;
+        try {
+          const { accessToken } = await refresh();
+          if (accessToken) {
+            tokenStorage.setToken(accessToken);
+            originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+            return client(originalRequest!);
+          }
+        } catch {
+          tokenStorage.clearToken();
+          window.location.reload();
+        }
+      }
+    }
+  },
+);

5405b033d000831bd448f626a303c2a859542d76
Ajungmiiin <wjdals3249@naver.com>
2026-01-20 19:18:48 +0900

    feat : addCustomSubscribe 함수 정의

--- src/api/subscribe.ts
+++ src/api/subscribe.ts
@@ -2,6 +2,7 @@ import type { AddSubscribeRequestType } from '@/schema/add-subscribe-schema';
 import { client } from './client';
 
 import {
+  type AddCustomSubscribeReq,
   type subscribeItemRes,
   type subscribeListRes,
 } from '@/types/subscribe';
@@ -25,6 +26,37 @@ export const addSubscribe = async (subscribeInfo: AddSubscribeRequestType) => {
     '/api/member-subscriptions',
     subscribeInfo,
   );
+
+  return response.data;
+};
+
+export const addCustomSubscribe = async (
+  subscribeInfo: AddCustomSubscribeReq,
+) => {
+  const formData = new FormData();
+
+  if (subscribeInfo.image) {
+    formData.append('image', subscribeInfo.image);
+  }
+  formData.append(
+    'request',
+    new Blob(
+      [JSON.stringify({ name: subscribeInfo.name, type: subscribeInfo.type })],
+      {
+        type: 'application/json',
+      },
+    ),
+  );
+
+  const response = await client.post<{ id: number }>(
+    '/api/subscriptions',
+    formData,
+  );
+  return response.data;
+};
+
+export const getSubscribeTypes = async () => {
+  const response = await client.get<string[]>('/api/subscriptions/types');
 
   return response.data;
 };

0df3b81cad203412adbc074303d386797d9472f3
Ajungmiiin <wjdals3249@naver.com>
2026-01-26 11:41:45 +0900

    feat : 라우트 경로 수정
    - add-subscribe etc... 경로 대신 subscribe/add, plan/add etc... 경로로 수정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,6 +11,9 @@ import AddSubscribeFormPage from './pages/add-subscribe-form-page';
 import MyPage from './pages/my-page';
 import AddCustomSubscribePage from './pages/add-custom-subscribe-page';
 import AddPlanPage from './pages/add-plan-page';
+import EditPlanPage from './pages/edit-plan-page';
+import PlanPageLayout from '@/components/layout/plan-page-layout';
+import PlanEditorPage from './pages/plan-editor-page';
 
 const router = createBrowserRouter([
   {
@@ -39,42 +42,69 @@ const router = createBrowserRouter([
           },
 
           {
-            path: '/add-subscribe',
-            element: <AddSubscribePage />,
-            handle: {
-              header: {
-                header: true,
-                title: '구독추가',
-                right: (
-                  <Link
-                    to="/add-custom-subscribe"
-                    className="bg-box-black rounded-full px-3.5 py-2.5 text-xs"
-                  >
-                    직접입력
-                  </Link>
-                ),
+            path: '/subscribe',
+            children: [
+              {
+                path: 'add',
+                element: <AddSubscribePage />,
+                handle: {
+                  header: {
+                    header: true,
+                    title: '구독추가',
+                    right: (
+                      <Link
+                        to="/subscribe/add/custom"
+                        className="bg-box-black rounded-full px-3.5 py-2.5 text-xs"
+                      >
+                        직접입력
+                      </Link>
+                    ),
+                  },
+                },
               },
-            },
-          },
-          {
-            path: '/add-custom-subscribe',
-            element: <AddCustomSubscribePage />,
-            handle: {
-              header: {
-                header: true,
+              {
+                path: 'add/:id',
+                element: <AddSubscribeFormPage />,
+                handle: { header: true },
               },
-            },
+              {
+                path: 'add/custom',
+                element: <AddCustomSubscribePage />,
+                handle: {
+                  header: {
+                    header: true,
+                  },
+                },
+              },
+              {
+                path: 'add/:subscribeId/plan',
+                element: <PlanPageLayout />,
+                children: [
+                  {
+                    path: 'add',
+                    element: <AddPlanPage />,
+                    handle: { header: true },
+                  },
+                  {
+                    path: 'edit',
+                    element: <PlanEditorPage />,
+                    handle: {
+                      header: {
+                        header: true,
+                        title: '멤버십 관리',
+                      },
+                    },
+                  },
+                  {
+                    path: 'edit/:planId',
+                    element: <EditPlanPage />,
+                    handle: { header: true },
+                  },
+                ],
+              },
+            ],
           },
-          {
-            path: '/add-subscribe/:id',
-            element: <AddSubscribeFormPage />,
-            handle: { header: true },
-          },
-          {
-            path: '/add-subscribe/:id/add-plan',
-            element: <AddPlanPage />,
-            handle: { header: true },
-          },
+
           {
             path: '/my',
             element: <MyPage />,

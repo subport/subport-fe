7a55218a115bbcf32254052f09e840ae16f7a7f3
Ajungmiiin <wjdals3249@naver.com>
2026-01-26 11:40:47 +0900

    feat : getPlan, updatePlan, deletePlan API 함수 추가

--- src/api/plan.ts
+++ src/api/plan.ts
@@ -1,10 +1,18 @@
-import { type AddPlanReq, type PlanList } from '@/types/plan';
+import type { PlanItem, AddPlanReq, PlanList } from '@/types/plan';
 import { client } from './client';
 
-export const getPlans = async (id: string) => {
-  const response = await client.get<PlanList>(`/api/subscriptions/${id}/plans`);
+export const getPlans = async (planId: string) => {
+  const response = await client.get<PlanList>(
+    `/api/subscriptions/${planId}/plans`,
+  );
 
   return response.data.plans;
+};
+
+export const getPlan = async (planId: string) => {
+  const response = await client.get<PlanItem>(`/api/plans/${planId}`);
+
+  return response.data;
 };
 
 export const addPlan = async ({
@@ -12,12 +20,38 @@ export const addPlan = async ({
   amountUnit,
   durationMonths,
   name,
-  id,
-}: AddPlanReq & { id: number }) => {
+  subscribeId,
+}: AddPlanReq & { subscribeId: number }) => {
   const response = await client.post<{ id: number }>(
-    `/api/subscriptions/${id}/plans`,
+    `/api/subscriptions/${subscribeId}/plans`,
     { amount: Number(amount), amountUnit, durationMonths, name },
   );
 
   return response.data;
+};
+
+export const updatePlan = async ({
+  amount,
+  amountUnit,
+  durationMonths,
+  name,
+  planId,
+}: AddPlanReq & { planId: number; subscribeId: number }) => {
+  const response = await client.put(`/api/plans/${planId}`, {
+    amount: Number(amount),
+    amountUnit,
+    durationMonths,
+    name,
+  });
+
+  return response.data;
+};
+
+export const deletePlan = async (ids: {
+  planId: number;
+  subscribeId: number;
+}) => {
+  const response = await client.delete(`/api/plans/${ids.planId}`);
+
+  return response;
 };

db833369a1c64f09cad90fdb12d7206efae13732
Ajungmiiin <wjdals3249@naver.com>
2026-02-26 02:12:01 +0900

    feat : 마이 페이지 관련 API 함수 추가

--- src/api/profile.ts
+++ src/api/profile.ts
@@ -1,8 +1,56 @@
-import type { MyProfileRes } from '@/types/profile';
+import {
+  type ReminderSettingsRes,
+  type MyAccountRes,
+  type MyProfileRes,
+} from '@/types/profile';
 import { client } from './client';
+import type { MyAccountReqType } from '@/schema/my-account-schema';
+
+type ReminderReqType =
+  | {
+      paymentReminderEnabled: true;
+      reminderDaysBefore: number;
+    }
+  | {
+      paymentReminderEnabled: false;
+    };
 
 export const getMyProfile = async () => {
   const response = await client.get<MyProfileRes>('/api/members/me/profile');
+
+  return response.data;
+};
+
+export const getMyAccount = async () => {
+  const response = await client.get<MyAccountRes>('/api/members/me');
+
+  return response.data;
+};
+
+export const updatedMyAccount = async (updatedAccount: MyAccountReqType) => {
+  const response = await client.put<MyAccountRes>(
+    '/api/members/me',
+    updatedAccount,
+  );
+
+  return response.data;
+};
+
+export const getReminderSettings = async () => {
+  const response = await client.get<ReminderSettingsRes>(
+    '/api/members/me/reminder-settings',
+  );
+
+  return response.data;
+};
+
+export const updatedReminderSettings = async (
+  reminderSettings: ReminderReqType,
+) => {
+  const response = await client.put<ReminderSettingsRes>(
+    '/api/members/me/reminder-settings',
+    reminderSettings,
+  );
 
   return response.data;
 };

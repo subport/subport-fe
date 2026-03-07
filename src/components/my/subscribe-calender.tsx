87f57e8857eb785e2696a64745f3fe34b5c3e719
Ajungmiiin <wjdals3249@naver.com>
2026-03-08 04:25:48 +0900

    feat : 캘린더 슬라이드 기능 추가, 지출 액 비교 문구 UI 수정

--- src/components/my/subscribe-calender.tsx
+++ src/components/my/subscribe-calender.tsx
@@ -1,11 +1,13 @@
 import 'react-day-picker/style.css';
 
+import { addMonths, isAfter, startOfMonth } from 'date-fns';
 import { ko } from 'date-fns/locale';
 import { DayPicker } from 'react-day-picker';
-import { formatKRWInput } from '@/lib/utils';
+import { cn, formatKRWInput } from '@/lib/utils';
 import CalenDerTrigger from '../calender/calender-trigger';
 import type { PaymentDates } from '@/api/calender';
 import SpendingStateDot from './spending-state-dot';
+import { useRef } from 'react';
 
 interface SubscribeCalenderProps {
   totalAmount: number;
@@ -26,6 +28,8 @@ function SubscribeCalender({
   selectedDay,
   paymentDates,
 }: SubscribeCalenderProps) {
+  const clientX = useRef(0);
+  const clientY = useRef(0);
   return (
     <>
       <CalenDerTrigger
@@ -36,69 +40,108 @@ function SubscribeCalender({
 
       <div className="py-5">
         <p className="text-2xl font-semibold">{`${formatKRWInput(totalAmount.toString())}원`}</p>
-        <p className="text-sm text-[#5EC3B6]">
+        <p
+          className={cn(
+            amountDiffFromPreviousMonth < 0
+              ? 'text-[#5EC3B6]'
+              : amountDiffFromPreviousMonth === 0
+                ? 'text-white'
+                : 'text-d-day-color-7day',
+            'text-sm',
+          )}
+        >
           {amountDiffFromPreviousMonth < 0
             ? `지난달보다 ${formatKRWInput(Math.abs(amountDiffFromPreviousMonth).toString())}원 줄었어요`
-            : `지난달보다 ${formatKRWInput(amountDiffFromPreviousMonth.toString())}원 늘었어요`}
+            : amountDiffFromPreviousMonth === 0
+              ? '지난달과 동일해요'
+              : `지난달보다 ${formatKRWInput(amountDiffFromPreviousMonth.toString())}원 늘었어요`}
         </p>
       </div>
 
-      <DayPicker
-        month={viewMonth}
-        required
-        showOutsideDays
-        animate
-        mode="single"
-        selected={selectedDay}
-        onSelect={onSelectDay}
-        onMonthChange={onMonthChange}
-        hideNavigation
-        locale={ko}
-        lang="ko-KR"
-        classNames={{
-          months: 'w-full',
-          month_grid: 'w-full',
-          outside: 'bg-background-black',
-          day: ' relative bg-box-black  rounded-lg w-1/7 border-2 border-box-black transition-all',
-          day_button:
-            'flex w-full h-full items-end  pt-5 [@media(min-width:390px)]:pt-7  [@media(min-width:430px)]:pt-8  pb-1 justify-center cursor-pointer',
-          week: 'flex justify-between gap-1.5',
-          weeks: 'space-y-1.5',
-          weekdays: 'flex justify-between',
-          weekday: 'w-[42px] text-sub-font-black font-medium text-sm py-2',
-          selected: 'border-2  border-white',
-          today: '',
+      <div
+        onTouchStart={(e) => {
+          clientX.current = e.touches[0].clientX;
+          clientY.current = e.touches[0].clientY;
         }}
-        components={{
-          Day: ({ day, children, ...rest }) => {
-            const date = day.date.getDate();
-            const hasSpending = !!paymentDates[date];
+        onTouchEnd={(e) => {
+          const endX = e.changedTouches[0]?.clientX;
+          const endY = e.changedTouches[0]?.clientY;
+          const deltaX = endX - clientX.current;
+          const deltaY = endY - clientY.current;
+          const swipeThreshold = 40;
 
-            return (
-              <>
-                <td {...rest}>
-                  {hasSpending && (
-                    <div className="absolute top-1.5 right-1.5 flex items-center gap-0.5">
-                      {paymentDates[date].hasSubscription && (
-                        <SpendingStateDot state={'ongoing'} />
-                      )}
-                      {paymentDates[date].hasSpending && (
-                        <SpendingStateDot state={'complete'} />
-                      )}
-                    </div>
-                  )}
+          if (
+            Math.abs(deltaX) < swipeThreshold ||
+            Math.abs(deltaX) <= Math.abs(deltaY)
+          ) {
+            return;
+          }
 
-                  {children}
-                </td>
-              </>
-            );
-          },
+          const nextMonth = addMonths(viewMonth, deltaX < 0 ? 1 : -1);
+
+          if (isAfter(startOfMonth(nextMonth), startOfMonth(new Date()))) {
+            return;
+          }
+          onSelectDay(undefined);
+          onMonthChange(nextMonth);
         }}
-        disabled={(date) =>
-          date.getMonth() !== viewMonth?.getMonth() ||
-          date.getFullYear() !== viewMonth?.getFullYear()
-        }
-      />
+      >
+        <DayPicker
+          month={viewMonth}
+          required
+          showOutsideDays
+          animate
+          mode="single"
+          selected={selectedDay}
+          onSelect={onSelectDay}
+          onMonthChange={onMonthChange}
+          hideNavigation
+          locale={ko}
+          lang="ko-KR"
+          classNames={{
+            months: 'w-full',
+            month_grid: 'w-full',
+            outside: 'bg-background-black',
+            day: ' relative bg-box-black  rounded-lg w-1/7 border-2 border-box-black transition-all',
+            day_button:
+              'flex w-full h-full items-end  pt-5 [@media(min-width:390px)]:pt-7  [@media(min-width:430px)]:pt-8  pb-1 justify-center cursor-pointer',
+            week: 'flex justify-between gap-1.5',
+            weeks: 'space-y-1.5',
+            weekdays: 'flex justify-between',
+            weekday: 'w-[42px] text-sub-font-black font-medium text-sm py-2',
+            selected: 'border-2  border-white',
+            today: '',
+          }}
+          components={{
+            Day: ({ day, children, ...rest }) => {
+              const date = day.date.getDate();
+              const hasSpending = !!paymentDates[date];
+              return (
+                <>
+                  <td {...rest}>
+                    {!day.outside && hasSpending && (
+                      <div className="absolute top-1.5 right-1.5 flex items-center gap-0.5">
+                        {paymentDates[date].hasSubscription && (
+                          <SpendingStateDot state={'ongoing'} />
+                        )}
+                        {paymentDates[date].hasSpending && (
+                          <SpendingStateDot state={'complete'} />
+                        )}
+                      </div>
+                    )}
+
+                    {children}
+                  </td>
+                </>
+              );
+            },
+          }}
+          disabled={(date) =>
+            date.getMonth() !== viewMonth?.getMonth() ||
+            date.getFullYear() !== viewMonth?.getFullYear()
+          }
+        />
+      </div>
     </>
   );
 }

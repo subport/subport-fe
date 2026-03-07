2bde6a11fd8bd1f926a1346b13f196c5e0f9575e
Ajungmiiin <wjdals3249@naver.com>
2026-03-08 04:21:49 +0900

    feat : 구독 내역 삭제 mutate 함수 추가

---
+++ src/hooks/mutations/use-delete-spending-record-mutate.ts
@@ -0,0 +1,31 @@
+import { deleteSpendingRecord } from '@/api/calender';
+import { queryClient } from '@/components/providers/query-provider';
+import { QUERY_KEY } from '@/constants/query-key';
+import { useMutation } from '@tanstack/react-query';
+import { format } from 'date-fns';
+
+type DeleteSpendingRecordMutateType = {
+  recordId: string;
+  date: Date;
+};
+
+function useDeleteSpendingRecordMutate() {
+  return useMutation({
+    mutationFn: ({ recordId }: DeleteSpendingRecordMutateType) =>
+      deleteSpendingRecord(recordId),
+    onSuccess: (_, variables) => {
+      const dateKey = format(variables.date, 'yyyy-MM-dd');
+      const monthKey = format(variables.date, 'yyyy-MM');
+
+      queryClient.invalidateQueries({
+        queryKey: QUERY_KEY.spendingRecords.byDate(dateKey),
+      });
+
+      queryClient.invalidateQueries({
+        queryKey: QUERY_KEY.spendingRecords.yearMonth(monthKey),
+      });
+    },
+  });
+}
+
+export default useDeleteSpendingRecordMutate;

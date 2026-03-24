c2117260e7088335f29517930b23d1cd900e5c95
Ajungmiiin <wjdals3249@naver.com>
2026-03-24 16:44:17 +0900

    feat : 구독 추가 후 spendingRecords 쿼리 무효화

--- src/hooks/mutations/use-add-subscribe-mutate.ts
+++ src/hooks/mutations/use-add-subscribe-mutate.ts
@@ -1,5 +1,6 @@
 import { addSubscribe } from '@/api/subscribe';
 import { queryClient } from '@/components/providers/query-provider';
+import { QUERY_KEY } from '@/constants/query-key';
 import type { useMutationCallbacks } from '@/types/mutate';
 import { useMutation } from '@tanstack/react-query';
 
@@ -7,6 +8,10 @@ export const useAddSubscribeMutate = (callbacks?: useMutationCallbacks) => {
   return useMutation({
     mutationFn: addSubscribe,
     onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: QUERY_KEY.spendingRecords.all,
+      });
+
       queryClient.invalidateQueries({
         queryKey: ['member-subscriptions'],
         predicate: (q) => {

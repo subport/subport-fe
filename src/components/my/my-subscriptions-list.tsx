12b9467418a2d797aa3f894ec64becce17a1b2e0
Ajungmiiin <wjdals3249@naver.com>
2026-03-29 23:10:13 +0900

    refactor: 내 구독 목록 페이지 상태 관리와 리스트 구조 정리

--- src/components/my/my-subscriptions-list.tsx
+++ src/components/my/my-subscriptions-list.tsx
@@ -1,7 +1,7 @@
 import useGetMemberSubscriptions from '@/hooks/queries/use-get-member-subscriptions';
-import SubscribeList from '../subscribe/member-subscribe/subscribe-list';
-import type { MemberSubscriptions } from '@/types/subscribe';
 import SubscribeListFallback from '../ui/fallback/subscribe-list-fallback';
+import UserSubscriptionList from '@/domains/subscription/user-subscription/components/user-subscription-list';
+import mappingUserSubscriptionList from '@/domains/subscription/user-subscription/model/mapper';
 
 function MySubscriptionsList() {
   const { data: subscriptions, isPending: isGetSubscriptionsPending } =
@@ -9,17 +9,20 @@ function MySubscriptionsList() {
 
   if (isGetSubscriptionsPending) return <SubscribeListFallback />;
   if (!subscriptions) return <p>목록 불러오기 실패</p>;
+
+  const userSubscriptionList = mappingUserSubscriptionList(
+    { active: true, sortBy: 'createdAt' },
+    subscriptions,
+  );
   return (
     <div className="scrollbar-hide h-full overflow-scroll pb-4">
-      {(subscriptions.subscriptions as MemberSubscriptions).length > 0 && (
+      {userSubscriptionList.sections.length > 0 && (
         <>
           <p className="mb-4 text-lg font-semibold">{`${subscriptions.subscriptions.length}건 구독 중`}</p>
-          <SubscribeList
-            subscribeList={subscriptions.subscriptions as MemberSubscriptions}
-          />
+          <UserSubscriptionList sections={userSubscriptionList.sections} />
         </>
       )}
-      {(subscriptions.subscriptions as MemberSubscriptions).length === 0 && (
+      {userSubscriptionList.sections.length === 0 && (
         <>
           <p className="text-sub-font-black flex h-full items-center justify-center">
             구독하고 있는 서비스가 존재하지 않습니다.
